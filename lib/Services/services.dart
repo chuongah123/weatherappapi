a9cc5598485337abcc771682fb8ccbea2c39e7b6
chuongah123 <chuong19032001@gmai.com>
2024-05-10 21:38:44 +0700

    weatherapi

---
+++ lib/Services/services.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:weatherapp/Model/weather_model.dart';
+
+class WeatherServices {
+  fetchWeather() async {
+    final response = await http.get(
+      Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?lat=21.028511&lon=105.804817&appid=509079b22fae7e954dff8403ef5eba0e"),
+    );
+    try {
+      if (response.statusCode == 200) {
+        var json = jsonDecode(response.body);
+        return WeatherData.fromJson(json);
+      } else {
+        throw Exception('Failed to load Weather data');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
